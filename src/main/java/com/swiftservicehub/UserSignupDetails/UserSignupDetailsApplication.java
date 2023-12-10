bafd0c28cf9c819720cbf3313f342c50f2f91edc
Princeshiva74482 <shiva74482@outlook.com>
2023-12-10 16:56:15 +0530

    Initial commit

---
+++ src/main/java/com/swiftservicehub/UserSignupDetails/UserSignupDetailsApplication.java
@@ -0,0 +1,20 @@
+package com.swiftservicehub.UserSignupDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSignupDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSignupDetailsApplication.class, args);
+	}
+
+}
+
+
+	git init
+	git add .
+		git commit -m "Initial commit"
+		git remote add origin https://github.com/swift-service-hub/User-Signup-Details.git
+		git push -u origin master
